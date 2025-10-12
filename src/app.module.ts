ebab88a8c92f02fdf832238f99cb910e110f8b25
ifrairshadmalikk <ifrairshadmalik@gmail.com>
2025-10-12 14:53:04 +0100

    Apis creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-<<<<<<< HEAD
 import { ConfigModule } from '@nestjs/config';
 import { PassengerModule } from './passenger/passenger.module';
 import { DriverModule } from './driver/driver.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './users/user.module';
 import { GuardianModule } from './guardian/guardian.module';
-import { TransporterModule } from './transporter/transporter.module'; // 👈 Added TransporterModule
+import { TransporterModule } from './transporter/transporter.module';
 
 @Module({
   imports: [
@@ -18,15 +17,9 @@ import { TransporterModule } from './transporter/transporter.module'; // 👈 Ad
     PassengerModule,
     DriverModule,
     GuardianModule,
-    TransporterModule, 
+    TransporterModule,
     UserModule,
     AuthModule,
-=======
-
-@Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/swiftlift_dev'), // local DB
->>>>>>> ebaec0106408cc835bc0f55d690ab4b7c38b0f6e
   ],
 })
 export class AppModule {}
